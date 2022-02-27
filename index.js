d8eeb97a97e87d62d220306eb59a3b0d1f39f8ac
Josette Garcia <josettetgarcia@gmail.com>
2022-02-27 14:39:24 -0600

    commit tree cc2681c1e4481bc6d124ff22d1c84efbc2989f30

--- index.js
+++ index.js
@@ -1,5 +1,64 @@
 /* Your Code Here */
 
+    function createEmployeeRecord(array){
+        let employeeRecord = {
+          firstName: array[0],
+          familyName: array[1], 
+          title: array[2], 
+          payPerHour: array[3], 
+          timeInEvents: [],
+          timeOutEvents: [], 
+        }
+        return employeeRecord;
+      }
+
+    function createEmployeeRecords(arrOfArrays){
+        return arrOfArrays.map( (array) => createEmployeeRecord(array))
+    }
+
+    function createTimeInEvent(event){
+        let [date, hour] = event.split(" ")
+        let eventObj = {
+            type: "TimeIn", 
+            date: date,
+            hour: parseInt(hour, 10),
+        }
+        this.timeInEvents.push(eventObj)
+        return this
+    }
+
+
+    function createTimeOutEvent(event){
+        let [date, hour] = event.split(" ")
+        let eventObj = {
+            type: "TimeOut",
+            date: date,
+            hour: parseInt(hour, 10),
+        }
+        this.timeOutEvents.push(eventObj)
+        return this
+    }
+
+    function hoursWorkedOnDate(date){
+        const timeIn = this.timeInEvents.find(event => event.date === date)
+        const timeOut = this.timeOutEvents.find(event => event.date === date)
+        return (timeOut.hour - timeIn.hour)/100
+    }
+
+    function wagesEarnedOnDate(date) {
+        const hours = hoursWorkedOnDate.call(this, date)
+        return this.payPerHour * hours
+    }
+ 
+function calculatePayroll(employeeRecords){
+   const record =  employeeRecords.map(employee => allWagesFor.call(employee))
+   return record.reduce((currentValue, total) => currentValue + total)
+}
+
+function findEmployeeByFirstName(employees, fistNameString) {
+    return employees.find(emp => emp.firstName === fistNameString)
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
